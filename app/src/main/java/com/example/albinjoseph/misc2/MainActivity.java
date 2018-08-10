8935fe6eedaa6ce2aeeb7c4e3093a8d5b1ba59ff
amous4822 <amous4822@gmail.com>
2018-08-11 02:54:09 +0530

    added sign in buttons and field and display info in log

--- app/src/main/java/com/example/albinjoseph/misc2/MainActivity.java
+++ app/src/main/java/com/example/albinjoseph/misc2/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.albinjoseph.misc2;
 
+import android.inputmethodservice.ExtractEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickFunction(View button){
+    public void clickFunction(View one){
 
-        Log.i("info - " , "button pressed !!");
+        EditText username =  findViewById(R.id.Name1);
+        EditText password =  findViewById(R.id.Pname1);
+
+        Log.i("main info" , username.getText().toString() + "  " +  password.getText().toString());
+
     }
 
     @Override
