ad4a83fb984755aa1b4d0e35919dfdeab1416877
amous4822 <amous4822@gmail.com>
2018-08-11 02:12:14 +0530

    added button

--- app/src/main/java/com/example/albinjoseph/misc2/MainActivity.java
+++ app/src/main/java/com/example/albinjoseph/misc2/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.albinjoseph.misc2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View button){
+
+        Log.i("info - " , "button pressed !!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
